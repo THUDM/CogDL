642cb3f50d6246c79e7ceb8a14b0e933e24809fa
Qibin Chen <chenqibin422@gmail.com>
2019-07-02 12:31:11 +0800

    Fix parallel training on multiple gpus

--- train.py
+++ train.py
@@ -1,6 +1,8 @@
 import copy
 import itertools
+import os
 import random
+import time
 from collections import defaultdict, namedtuple
 
 import numpy as np
@@ -16,19 +18,27 @@ from cognitive_graph.tasks import build_task
 
 def main(args):
     if torch.cuda.is_available() and not args.cpu:
-        torch.cuda.set_device(args.device_id)
+        pid = mp.current_process().pid
+        torch.cuda.set_device(args.pid_to_cuda[pid])
 
     random.seed(args.seed)
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
 
     task = build_task(args)
-    return task.train()
+    result = task.train()
+    return result
 
 
 def gen_variants(**items):
     Variant = namedtuple("Variant", items.keys())
     return itertools.starmap(Variant, itertools.product(*items.values()))
+
+
+def getpid(_):
+    # HACK to get different pids
+    time.sleep(1)
+    return mp.current_process().pid
 
 
 if __name__ == "__main__":
@@ -39,7 +49,9 @@ if __name__ == "__main__":
     args, _ = parser.parse_known_args()
     args = options.parse_model_and_add_parameter(parser, args)
     print(args)
-    variants = gen_variants(dataset=args.dataset, model=args.model, seed=args.seed)
+    variants = list(
+        gen_variants(dataset=args.dataset, model=args.model, seed=args.seed)
+    )
     results_dict = defaultdict(list)
 
     device_ids = args.device_id
@@ -50,21 +62,28 @@ if __name__ == "__main__":
     print("num_workers", num_workers)
 
     with mp.Pool(processes=num_workers) as pool:
-        for idx, variant in enumerate(variants):
-            args.device_id = device_ids[idx % num_workers]
-            args.dataset, args.model, args.seed = variant
-            results_dict[variant[:-1]].append(
-                pool.apply_async(func=main, args=(copy.copy(args),))
-            )
-        pool.close()
-        pool.join()
+        # Map process to cuda device
+        pids = pool.map(getpid, range(num_workers))
+        pid_to_cuda = dict(zip(pids, device_ids))
+        # yield all variants
+        def variant_args_generator():
+            """Form variants as group with size of num_workers"""
+            for variant in variants:
+                args.pid_to_cuda = pid_to_cuda
+                args.dataset, args.model, args.seed = variant
+                yield copy.deepcopy(args)
+
+        # Collect results
+        results = pool.map(main, variant_args_generator())
+        for variant, result in zip(variants, results):
+            results_dict[variant[:-1]].append(result)
 
     # Average for different seeds
-    col_names = ["Variant"] + list(results_dict[variant[:-1]][-1].get().keys())
+    col_names = ["Variant"] + list(results_dict[variant[:-1]][-1].keys())
 
     tab_data = []
     for variant in results_dict:
-        results = np.array([list(res.get().values()) for res in results_dict[variant]])
+        results = np.array([list(res.values()) for res in results_dict[variant]])
         tab_data.append(
             [variant]
             + list(

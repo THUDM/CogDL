e362d3f6ff3067cbafe2ab5e15c72929098906c3
侯振宇 <houzy17@mails.tsinghua.edu.cn>
2020-05-10 22:35:23 +0800

    add datasets mutag/imdb/collab/proteins

--- cogdl/datasets/pyg.py
+++ cogdl/datasets/pyg.py
@@ -3,7 +3,7 @@ import os.path as osp
 import torch
 
 import torch_geometric.transforms as T
-from torch_geometric.datasets import Planetoid, Reddit
+from torch_geometric.datasets import Planetoid, Reddit, TUDataset
 
 from . import register_dataset
 
@@ -13,6 +13,8 @@ class CoraDataset(Planetoid):
     def __init__(self):
         dataset = "Cora"
         path = osp.join(osp.dirname(osp.realpath(__file__)), "../..", "data", dataset)
+        if not osp.exists(path):
+            Planetoid(path, dataset, T.NormalizeFeatures())
         super(CoraDataset, self).__init__(path, dataset, T.TargetIndegree())
 
 
@@ -21,6 +23,8 @@ class CiteSeerDataset(Planetoid):
     def __init__(self):
         dataset = "CiteSeer"
         path = osp.join(osp.dirname(osp.realpath(__file__)), "../..", "data", dataset)
+        if not osp.exists(path):
+            Planetoid(path, dataset, T.NormalizeFeatures())
         super(CiteSeerDataset, self).__init__(path, dataset, T.TargetIndegree())
 
 
@@ -29,6 +33,8 @@ class PubMedDataset(Planetoid):
     def __init__(self):
         dataset = "PubMed"
         path = osp.join(osp.dirname(osp.realpath(__file__)), "../..", "data", dataset)
+        if not osp.exists(path):
+            Planetoid(path, dataset, T.NormalizeFeatures())
         super(PubMedDataset, self).__init__(path, dataset, T.TargetIndegree())
 
 
@@ -37,4 +43,56 @@ class RedditDataset(Reddit):
     def __init__(self):
         dataset = "Reddit"
         path = osp.join(osp.dirname(osp.realpath(__file__)), "../..", "data", dataset)
+        if not osp.exists(path):
+            Reddit(path)
         super(RedditDataset, self).__init__(path, T.TargetIndegree())
+
+
+@register_dataset("mutag")
+class MUTAGDataset(TUDataset):
+    def __init__(self):
+        dataset = "MUTAG"
+        path = osp.join(osp.dirname(osp.realpath(__file__)), "../..", "data", dataset)
+        if not osp.exists(path):
+            TUDataset(path, name=dataset)
+        super(MUTAGDataset, self).__init__(path, name=dataset)
+
+
+@register_dataset("imdb-b")
+class ImdbBinaryDataset(TUDataset):
+    def __init__(self):
+        dataset = "IMDB-BINARY"
+        path = osp.join(osp.dirname(osp.realpath(__file__)), "../..", "data", dataset)
+        if not osp.exists(path):
+            TUDataset(path, name=dataset)
+        super(ImdbBinaryDataset, self).__init__(path, name=dataset)
+
+
+@register_dataset("imdb-m")
+class ImdbMultiDataset(TUDataset):
+    def __init__(self):
+        dataset = "IMDB-MULTI"
+        path = osp.join(osp.dirname(osp.realpath(__file__)), "../..", "data", dataset)
+        if not osp.exists(path):
+            TUDataset(path, name=dataset)
+        super(ImdbMultiDataset, self).__init__(path, name=dataset)
+
+
+@register_dataset("collab")
+class CollabDataset(TUDataset):
+    def __init__(self):
+        dataset = "COLLAB"
+        path = osp.join(osp.dirname(osp.realpath(__file__)), "../..", "data", dataset)
+        if not osp.exists(path):
+            TUDataset(path, name=dataset)
+        super(CollabDataset, self).__init__(path, name=dataset)
+
+
+@register_dataset("proteins")
+class ProtainsDataset(TUDataset):
+    def __init__(self):
+        dataset = "PROTEINS"
+        path = osp.join(osp.dirname(osp.realpath(__file__)), "../..", "data", dataset)
+        if not osp.exists(path):
+            TUDataset(path, name=dataset)
+        super(ProtainsDataset, self).__init__(path, name=dataset)

d72e660bf8896b7f035e0927ca8fb23b1a551975
Malin <szyszka45@gmail.com>
2020-11-28 01:13:15 +0100

    cleanup and tests

--- cogdl/tasks/attributed_graph_clustering.py
+++ cogdl/tasks/attributed_graph_clustering.py
@@ -1,27 +1,24 @@
-import copy
-import random
-from typing import Optional
-
+import argparse
+from typing import Dict
 import numpy as np
 import networkx as nx
 from sklearn.cluster import KMeans, SpectralClustering
 from sklearn.metrics.cluster import normalized_mutual_info_score
 import torch
 import torch.nn.functional as F
-from tqdm import tqdm
 
-from cogdl import options
 from cogdl.datasets import build_dataset
 from cogdl.models import build_model
 
 from . import BaseTask, register_task
+
 
 @register_task("attributed_graph_clustering")
 class AttributedGraphClustering(BaseTask):
     """Attributed graph clustring task."""
 
     @staticmethod
-    def add_args(parser):
+    def add_args(parser: argparse.ArgumentParser):
         """Add task-specific arguments to the parser."""
         # fmt: off
         # parser.add_argument("--num-features", type=int)
@@ -32,7 +29,9 @@ class AttributedGraphClustering(BaseTask):
         parser.add_argument("--momentum", type=float, default=0)
         # fmt: on
 
-    def __init__(self, args, dataset=None, model=None,):
+    def __init__(
+        self, args, dataset=None, model=None,
+    ):
         super(AttributedGraphClustering, self).__init__(args)
 
         self.args = args
@@ -46,7 +45,7 @@ class AttributedGraphClustering(BaseTask):
 
         self.hidden_size = args.hidden_size = dataset.num_features
         self.model = build_model(args)
-        
+
         self.momentum = args.momentum
         self.num_clusters = args.num_clusters
         if not args.cluster_method in ["kmeans", "spectral"]:
@@ -58,7 +57,7 @@ class AttributedGraphClustering(BaseTask):
         self.is_weighted = self.data.edge_attr is not None
         self.enhance = args.enhance
 
-    def train(self):
+    def train(self) -> Dict[str, float]:
         if self.model_type == "emb":
             G = nx.Graph()
             if self.is_weighted:
@@ -66,9 +65,7 @@ class AttributedGraphClustering(BaseTask):
                     self.data.edge_index.t().tolist(),
                     self.data.edge_attr.tolist(),
                 )
-                G.add_weighted_edges_from(
-                    [(edges[i][0], edges[i][1], weight[i][0]) for i in range(len(edges))]
-                )
+                G.add_weighted_edges_from([(edges[i][0], edges[i][1], weight[i][0]) for i in range(len(edges))])
             else:
                 G.add_edges_from(self.data.edge_index.t().tolist())
             embeddings = self.model.train(G)
@@ -82,21 +79,21 @@ class AttributedGraphClustering(BaseTask):
             features_matrix = F.normalize(features_matrix, p=2, dim=1)
             node_attr = F.normalize(self.data.x, p=2, dim=1)
             features_matrix = self.momentum * node_attr + (1 - self.momentum) * features_matrix
-        #TODO: add gnn training methods
-            
+        # TODO: add gnn training methods
+
         features_matrix = features_matrix.numpy()
         print("Clustering...")
         if self.cluster_method == "kmeans":
             kmeans = KMeans(n_clusters=self.num_clusters, random_state=0).fit(features_matrix)
             clusters = kmeans.labels_
         else:
-            #features_matrix = np.dot(features_matrix, features_matrix.transpose())
-            #features_matrix = 0.5 * (np.abs(features_matrix) + np.abs(features_matrix.transpose()))
+            # features_matrix = np.dot(features_matrix, features_matrix.transpose())
+            # features_matrix = 0.5 * (np.abs(features_matrix) + np.abs(features_matrix.transpose()))
             clustering = SpectralClustering(n_clusters=self.num_clusters, assign_labels="kmeans", random_state=0).fit(features_matrix)
             clusters = clustering.labels_
-        return self.evaluate(clusters)
+        return self.__evaluate(clusters)
 
-    def evaluate(self, clusters):
+    def __evaluate(self, clusters) -> Dict[str, float]:
         print("Evaluating...")
         TP = 0
         FP = 0
--- tests/tasks/test_attributed_graph_clustering.py
+++ tests/tasks/test_attributed_graph_clustering.py
@@ -1,6 +1,10 @@
+from cogdl.tasks.attributed_graph_clustering import AttributedGraphClustering
 import torch
 from cogdl.tasks import build_task
 from cogdl.utils import build_args_from_dict
+import unittest
+
+graph_clustering_task_name = "attributed_graph_clustering"
 
 
 def get_default_args():
@@ -12,23 +16,50 @@ def get_default_args():
         "hidden_size": 16,
         "model_type": "emb",
         "momentum": 0,
-        'enhance': None,
+        "enhance": None,
         "cpu": not cuda_available,
     }
     return build_args_from_dict(default_dict)
 
-def test_prone_cora():
+
+def create_simple_task():
     args = get_default_args()
-    args.task = "attributed_graph_clustering"
+    args.task = graph_clustering_task_name
     args.dataset = "cora"
     args.model = "prone"
     args.step = 5
     args.theta = 0.5
     args.mu = 0.2
-    task = build_task(args)
-    ret = task.train()
-    assert 0 <= ret["Accuracy"] <= 1
+    return AttributedGraphClustering(args)
+
+
+class TestAttributedGraphClustering(unittest.TestCase):
+    simple_task = None  # prone cora task
+    simple_task_train_result = None
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        if not cls.simple_task:
+            cls.simple_task = create_simple_task()
+            cls.simple_task_train_result = cls.simple_task.train()
+
+    def test_accuracy_within_range(self):
+        assert 0 <= self.__class__.simple_task_train_result["Accuracy"] <= 1
+
+    def test_micro_f1_within_range(self):
+        assert 0 <= self.__class__.simple_task_train_result["Micro_F1"] <= 1
+
+    def test_nmi_within_range(self):
+        assert 0 <= self.__class__.simple_task_train_result["NMI"] <= 1
+
+    def test_all_metrics_present(self):
+        assert "Accuracy" in self.__class__.simple_task_train_result
+        assert "NMI" in self.__class__.simple_task_train_result
+        assert "Micro_F1" in self.__class__.simple_task_train_result
+
+    def test_correct_class_resolved_from_build_task(self):
+        assert isinstance(self.__class__.simple_task, AttributedGraphClustering)
 
 
 if __name__ == "__main__":
-    test_prone_cora()
+    unittest.main()

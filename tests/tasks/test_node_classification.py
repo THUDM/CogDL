48a7e1a22c57b5d2dae951565d0206af0936b40e
kwyoke <44533444+kwyoke@users.noreply.github.com>
2020-11-29 16:07:28 +0800

    GDC_GCN model added, example script to run model also added (#69)
    * gdc_gcn model and example script added, match.yml modified
    
    * edit match.yml
    
    * shifted example script to scorrect folder
    
    * added unit test
    
    * modified unit test

--- tests/tasks/test_node_classification.py
+++ tests/tasks/test_node_classification.py
@@ -23,6 +23,27 @@ def get_default_args():
     return build_args_from_dict(default_dict)
 
 
+def test_gdc_gcn_cora():
+    args = get_default_args()
+    args.task = 'node_classification'
+    args.dataset = 'cora'
+    args.model = 'gdc_gcn'
+    dataset = build_dataset(args)
+    args.num_features = dataset.num_features
+    args.num_classes = dataset.num_classes
+    args.num_layers = 1
+    args.alpha = 0.05 # ppr filter param
+    args.t = 5.0 # heat filter param
+    args.k = 128 # top k entries to be retained
+    args.eps = 0.01 # change depending on gdc_type
+    args.dataset = dataset
+    args.gdc_type = 'ppr' # ppr, heat, none
+
+    model = build_model(args)
+    task = build_task(args, dataset=dataset, model=model)
+    ret = task.train()
+    assert 0 <= ret["Acc"] <= 1
+
 def test_gcn_cora():
     args = get_default_args()
     args.task = "node_classification"
@@ -376,6 +397,7 @@ def test_sgcpn_cora():
 
 
 if __name__ == "__main__":
+    test_gdc_gcn_cora()
     test_gcn_cora()
     test_gat_cora()
     test_mlp_pubmed()

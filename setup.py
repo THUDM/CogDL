52132affd3cc2e831c8fba7192616a6c6d4e1cf6
Sahandfer <karimiali0022@yahoo.com>
2020-12-12 20:25:39 +0800

    Updated dependancies

--- setup.py
+++ setup.py
@@ -13,23 +13,19 @@ setup(
     long_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/THUDM/cogdl",
-
     license="MIT",
-
     classifiers=[
         # How mature is this project? Common values are
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
         "Development Status :: 3 - Alpha",
-
         # Indicate who your project is intended for
         "Intended Audience :: Science/Research",
         "Intended Audience :: Developers",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         # Pick your license as you wish (should match "license" above)
         "License :: OSI Approved :: MIT License",
-
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         "Programming Language :: Python :: 3",
@@ -39,7 +35,6 @@ setup(
     ],
     # What does your project relate to?
     keywords="network embedding, graph representation learning, graph neural networks",
-
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
     packages=find_packages(exclude=["tests", "tests.*", "examples", "examples.*"]),
@@ -66,12 +61,18 @@ setup(
         "texttable",
         "gensim",
         "ogb",
+        "emoji",
+        "pyinquirer",
     ],
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
     # $ pip install -e .[dev,test]
-    extra_require={"test": ["pytest",]}
+    extra_require={
+        "test": [
+            "pytest",
+        ]
+    }
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.

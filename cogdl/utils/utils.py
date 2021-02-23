5f475a2db156cdc15fc3f02147ebc9dcbf28c483
Yukuo Cen <cenyk1230@qq.com>
2021-02-23 19:33:55 +0800

    Fix makedirs

--- cogdl/utils/utils.py
+++ cogdl/utils/utils.py
@@ -49,6 +49,14 @@ def untar(path, fname, deleteTar=True):
         os.remove(fullpath)
 
 
+def makedirs(path):
+    try:
+        os.makedirs(osp.expanduser(osp.normpath(path)))
+    except OSError as e:
+        if e.errno != errno.EEXIST and osp.isdir(path):
+            raise e
+
+
 def download_url(url, path, name=None, log=True, redownload=False):
     """
     Downloads file using `requests`. If ``redownload`` is set to false, then

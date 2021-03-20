3759cbabf2762593804c491c67a13d3b6504cbaf
Kaixuan Ji <jkx19@mails.tsinghua.edu.cn>
2021-03-20 22:16:34 +0800

    can run now

--- cogdl/tasks/oagbert_tasks.py
+++ cogdl/tasks/oagbert_tasks.py
@@ -15,72 +15,15 @@ from cogdl.oag.oagbert_metainfo import OAGMetaInfoBertModel
 from cogdl.oag.oagbert import OAGBertPretrainingModel
 # from cogdl.oag.utils_389 import print_instance, MultiProcessTqdm, write_success, check_success
 from transformers import BertTokenizer
+from collections import Counter
 
 from cogdl.utils import download_url, untar
-# from cogdl import oagbert
-# can import line 71 in oagert.py
 from . import BaseTask, register_task
-
-PRETRAINED_MODEL_ARCHIVE_MAP = {
-    "oagbert-v1": "https://cloud.tsinghua.edu.cn/f/051c9f87d8544698826e/?dl=1",
-    "oagbert-test": "https://cloud.tsinghua.edu.cn/f/68a8d42802564d43984e/?dl=1",
-    "oagbert-v2-test": "https://cloud.tsinghua.edu.cn/f/baff5abe84c4483bb690/?dl=1",
-    "oagbert-v2": "https://cloud.tsinghua.edu.cn/f/f06448fa3c234317bd16/?dl=1",
-}
-
-# class OAGBertPretrainingModel(BertForPreTrainingPreLN):
-#     def __init__(self, bert_config):
-#         super(OAGBertPretrainingModel, self).__init__(bert_config)
-
-#     def forward(
-#         self,
-#         input_ids,
-#         token_type_ids=None,
-#         attention_mask=None,
-#         output_all_encoded_layers=False,
-#         checkpoint_activations=False,
-#     ):
-#         return self.bert.forward(
-#             input_ids=input_ids,
-#             token_type_ids=token_type_ids,
-#             attention_mask=attention_mask,
-#             output_all_encoded_layers=output_all_encoded_layers,
-#             checkpoint_activations=checkpoint_activations,
-#         )
-
-#     @staticmethod
-#     def _load(model_name_or_path: str, load_weights: bool = False):
-#         if not os.path.exists(model_name_or_path):
-#             if model_name_or_path in PRETRAINED_MODEL_ARCHIVE_MAP:
-#                 if not os.path.exists(f"saved/{model_name_or_path}"):
-#                     archive_file = PRETRAINED_MODEL_ARCHIVE_MAP[model_name_or_path]
-#                     download_url(archive_file, "saved/", f"{model_name_or_path}.zip")
-#                     untar("saved/", f"{model_name_or_path}.zip")
-#                 model_name_or_path = f"saved/{model_name_or_path}"
-#             else:
-#                 raise KeyError("Cannot find the pretrained model {}".format(model_name_or_path))
-
-#         try:
-#             version = open(os.path.join(model_name_or_path, "version")).readline().strip()
-#         except Exception:
-#             version = None
-
-#         bert_config = BertConfig.from_dict(json.load(open(os.path.join(model_name_or_path, "bert_config.json"))))
-#         tokenizer = BertTokenizer.from_pretrained(model_name_or_path)
-#         if version == "2":
-#             bert_model = OAGMetaInfoBertModel(bert_config, tokenizer)
-#         else:
-#             bert_model = OAGBertPretrainingModel(bert_config)
-
-#         model_weight_path = os.path.join(model_name_or_path, "pytorch_model.bin")
-#         if load_weights and os.path.exists(model_weight_path):
-#             bert_model.load_state_dict(torch.load(model_weight_path))
-
-#         return bert_config, tokenizer, bert_model
 
 # from the code in run_decode_test.py
 
 def get_span_decode_prob(model, tokenizer, model_name, title='', abstract='', venue='', authors=[], concepts=[], affiliations=[], force_forward=False, span_type='', span='', debug=False, max_seq_length=512, device=None, wprop=False, wabs=False):
+    # print('get_span_decode_prob called')
     token_type_str_lookup = ['TEXT', 'AUTHOR', 'VENUE', 'AFF', 'FOS']
     input_ids = []
     input_masks = []
@@ -184,8 +127,8 @@ def get_span_decode_prob(model, tokenizer, model_name, title='', abstract='', ve
 
     return np.exp(logprobs), logproblist
 
-
-def calculate_samples(model, model_name, tokenizer, input_dir, sample_file, output_file, pbar: tqdm, debug=False, device=None, wprop=False, wabs=False, token_type='FOS', force_forward=False):
+def calculate_samples(model, model_name, tokenizer, input_dir, sample_file, output_file, debug=False, device=None, wprop=False, wabs=False, token_type='FOS', force_forward=False):
+    # print('calculate_samples called')
     candidates = []
     with open('%s/._SUCCESS' % input_dir) as f:
         for line in f:
@@ -230,63 +173,14 @@ def calculate_samples(model, model_name, tokenizer, input_dir, sample_file, outp
     fout.close()
     # pbar.close()
 
-def process_file(cuda_idx, input_dir, sample_file, output_file, model_name, wprop=False, wabs=False, token_type='FOS', debug=False, force_forward=False):
-    print('process_file called')
-    # # if check_success(output_file):
-    # #     return
-    # # pbar.reset(1, name='loading model')
-    # if model_name != 'scibert':
-    #     model_weights_path = get_model_weights_path(epoch_name=model_name)
-    #     configs = json.load(open('/home/cognitive/pretraining/workspace/oagbert_pretrain/train_metainfo_config.json'))
-    #     args, _ = argparse.ArgumentParser().parse_known_args()
-    #     args.deepspeed_transformer_kernel = False
-    #     args.deepspeed_sparse_attention = False
-    #     bert_config = NvidiaBertConfig.from_dict(configs['bert_model_config'])
-    #     bert_config.vocab_size = 44000
-    #     model = BertForPreTrainingPreLN(bert_config, args)
-    #     params = torch.load(model_weights_path, map_location=torch.device('cpu'))['module']
-    #     model.load_state_dict(params)
-    #     tokenizer = BertTokenizerFast.from_pretrained('/home/cognitive/yinda/oag-redis/data/oagbert_v1')
-    # # else:
-    # #     model = BertForPreTraining.from_pretrained('/mnt/V2OAG/models/scibert_scivocab_uncased')
-    # #     model.cls.predictions.decoder.bias = None
-    # #     tokenizer = BertTokenizer.from_pretrained('/mnt/V2OAG/models/scibert_scivocab_uncased')
-    # model.eval()
-    # device = torch.device('cuda:%d' % cuda_idx if cuda_idx >= 0 else 'cpu')
-    # model.to(device)
-    # calculate_samples(model=model, model_name=model_name, tokenizer=tokenizer, input_dir=input_dir, sample_file=sample_file, output_file=output_file, pbar=pbar, debug=debug, device=device, wprop=wprop, wabs=wabs, token_type=token_type, force_forward=force_forward)
-    # # write_success(output_file)
-
-# def run_mapred(args):
-
-#     # input_dir, output_dir, n_cpu, debug, model_name, wprop, wabs, token_type, force_forward
-
-#     input_dir = args.input_dir
-#     os.makedirs(args.output_dir, exist_ok=True)
-#     results = []
-#     idx = 0
-#     for filename in os.listdir(args.input_dir):
-#         if not filename.endswith('.jsonl'):
-#             continue
-#         infile = args.input_dir + '/' + filename
-#         outfile = args.output_dir + '/' + filename
-
-#         if not args.debug:
-#             process_file(idx % args.n_gpu if not args.debug else -1, args.input_dir, infile, outfile, args.model_name, args.wprop, args.wabs, args.token_type, False, args.force_forward)
-#             # results.append((r, filename))
-#         else:
-#             process_file(idx % args.n_gpu, args.input_dir, infile, outfile, args.model_name, args.wprop, args.wabs, args.token_type, True, args.force_forward)
-#         idx += 1
-#     # finished = set()
-#     # while len(finished) < len(results):
-#     #     for r, filename in results:
-#     #         if filename not in finished:
-#     #             if r.ready():
-#     #                 r.get()
-#     #                 finished.add(filename)
-#     #                 # summary_pbar.update(1)
-#     #     time.sleep(1)
-#     # pool.close()
+def process_file(model, tokenizer, cuda_idx, input_dir, sample_file, output_file, model_name, wprop=False, wabs=False, token_type='FOS', debug=False, force_forward=False):
+    # print('process_file called')
+    # print(model)
+    model.eval()
+    device = torch.device('cuda:%d' % cuda_idx if cuda_idx >= 0 else 'cpu')
+    model.to(device)
+    calculate_samples(model=model, model_name=model_name, tokenizer=tokenizer, input_dir=input_dir, sample_file=sample_file, output_file=output_file, debug=debug, device=device, wprop=wprop, wabs=wabs, token_type=token_type, force_forward=force_forward)
+    # write_success(output_file)
 
 
 @register_task("oagbert_tasks")
@@ -308,24 +202,20 @@ class zero_shot_inference(BaseTask):
 
     def __init__(self, args):
         super(zero_shot_inference, self).__init__(args)
-
-        # required args: dateset, model(SciBert?), task, version?, result-analising?, ...?
-        # args include: dataset, model(oagbert (with version?)), task(0shot), 
         '''
         argues to add when run the core function: 
         model, tokenizer, model-name(not necesary), title, abstract, 
         venue, authors=list, concepts=list, affiliations=list, 
         force_forward=False, span_type='', span='', debug=False, max_seq_length=512, device=None, wprop=False, wabs=False
         '''
-        self.args = args
-        # self.add_args(args)
-        print(f'The arguements are: {args}')
+        # print(f'The arguements are: {args}')
         # args: checkpoint=False, cpu=False, dataset='l0fos', debug=False, device_id=[0], eval_step=1, fast_spmm=False, force_forward=False, lr=0.01, max_epoch=500, model='oagbert', n_gpu=8, patience=100, samples_per_class=1000, save_dir='.', seed=1, task='oagbert_tasks', token_type='FOS', trainer=None, use_best_config=False, wabs=False, weight_decay=0.0005, wprop=False
 
+        self.dataset = args.dataset
         self.src_dir = 'saved/testset/%s' % args.dataset
         self.input_dir = 'saved/oagbert_v1.5/%s/samples' % args.dataset
         self.output_dir = 'saved/oagbert_v1.5/%s/results/%s-%sprop-%sabs%s%s' % (args.dataset, args.model, 'w' if args.wprop else 'n', 'w' if args.wabs else 'n', '' if not args.force_forward else '_ff', '' if not args.debug else '_debug')
-        self.load_model("oagbert-v1", True)
+        self.load_model("oagbert-v2", True)
 
         self.n_gpu = args.n_gpu
         self.debug = args.debug
@@ -336,7 +226,7 @@ class zero_shot_inference(BaseTask):
         self.force_forward = args.force_forward
         self.samples_per_class = args.samples_per_class
 
-        print('oagbert')
+        # print('oagbert')
         self.make_data()
 
         # if args.model == 'oagbert':
@@ -345,7 +235,7 @@ class zero_shot_inference(BaseTask):
         
             
     def train(self):
-        print('train function called')
+        # print('train function called')
         input_dir = self.input_dir
         os.makedirs(self.output_dir, exist_ok=True)
         results = []
@@ -357,11 +247,14 @@ class zero_shot_inference(BaseTask):
             outfile = self.output_dir + '/' + filename
 
             if not self.debug:
-                process_file(idx % self.n_gpu if not self.debug else -1, self.input_dir, infile, outfile, self.model_name, self.wprop, self.wabs, self.token_type, False, self.force_forward)
-                # results.append((r, filename))
+                process_file(self.model, self.tokenizer,idx % self.n_gpu if not self.debug else -1, self.input_dir, infile, outfile, self.model_name, self.wprop, self.wabs, self.token_type, False, self.force_forward)
             else:
-                process_file(idx % self.n_gpu, self.input_dir, infile, outfile, self.model_name, self.wprop, self.wabs, self.token_type, True, self.force_forward)
+                process_file(self.model, self.tokenizer, idx % self.n_gpu, self.input_dir, infile, outfile, self.model_name, self.wprop, self.wabs, self.token_type, True, self.force_forward)
             idx += 1
+            if idx% 100 == 0:
+                print(idx)
+
+        return self.analysis_result()
     
     
     def make_data(self):
@@ -400,6 +293,52 @@ class zero_shot_inference(BaseTask):
                 f.write('%s\n' % clz)
 
     def load_model(self, model_name_or_path: str, load_weights: bool):
-        print(model_name_or_path)
+        # print(model_name_or_path)
         _, self.tokenizer, self.model = OAGBertPretrainingModel._load(model_name_or_path, load_weights)
-        print('model loaded')
+        # print('model loaded')
+
+    def analysis_result(self):
+        result_dir = 'saved/oagbert_v1.5/%s/results/%s-%sprop-%sabs%s%s' % (self.dataset, self.model_name, 'w' if self.wprop else 'n', 'w' if self.wabs else 'n', '' if not self.force_forward else '_ff', '' if not self.debug else '_debug')
+        concepts = []
+        for filename in os.listdir(result_dir):
+            fos = filename.split('.')[0]
+            concepts.append(fos)
+        concepts.sort()
+
+        result = {}
+
+        T, F = 0, 0
+
+        for filename in os.listdir(result_dir):
+            if not filename.endswith('.jsonl'):
+                continue
+
+            fos = filename.split('.')[0]
+            if fos not in concepts:
+                continue
+            t, f = 0, 0
+            cnter = Counter()
+            for row in open('%s/%s' % (result_dir, filename)):
+                try:
+                    probs = json.loads(row.strip())['probs']
+                    pred = [k for k, v in sorted([(k, v) for k, v in probs if k in concepts], key=lambda tup: -tup[1])[:2]]
+                    # pred = json.loads(row.strip())['pred']
+
+                except Exception as e:
+                    print('Err:%s' % e)
+                    print('Row:%s' % row)
+                correct = pred[0] == fos #or pred[1] == fos
+                t += correct
+                f += not correct
+                cnter[pred[0]] += 1
+                # cnter[pred[1]] += 1
+            # print('%25s T/F/A:%4d/%4d/%5.2f%% %s' % (fos, t, f, t * 100 / (t + f), ', '.join(['%s:%.2f' % (key, value / (t+f)) for key, value in cnter.most_common(5)])))
+            result[fos] = t * 100 / (t + f)
+                        
+            T += t
+            F += f
+        
+        # print('Total T/F/A:%d/%d/%.2f%%' % (T, F, T * 100 / (T+F)))
+        result['total'] = T * 100 / (T+F)
+        print('result: ' + str(result))
+        return result

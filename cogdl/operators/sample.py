4138d250d8d745bd47e77af738342c989216489a
Yukuo Cen <cenyk1230@qq.com>
2021-04-12 15:32:43 +0800

    Fix exception message (#219)

--- cogdl/operators/sample.py
+++ cogdl/operators/sample.py
@@ -10,8 +10,8 @@ try:
     sample_adj_c = sample.sample_adj
     coo2csr_cpu = sample.coo2csr_cpu
     coo2csr_cpu_index = sample.coo2csr_cpu_index
-except Exception as e:
-    print(e)
+except Exception:
+    print("Failed to load C version of sampling, use python version instead.")
     subgraph_c = None
     sample_adj_c = None
     coo2csr_cpu_index = None

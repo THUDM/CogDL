672dd8bf4550e8a3ec7a5a67a4fdcbda30a56cd9
Yukuo Cen <cenyk1230@qq.com>
2021-05-28 19:18:39 +0800

    Formatting

--- examples/simple_hgn/conv.py
+++ examples/simple_hgn/conv.py
@@ -9,26 +9,30 @@ from dgl._ffi.base import DGLError
 from dgl.nn.pytorch.utils import Identity
 from dgl.utils import expand_as_pair
 
+
 # pylint: enable=W0235
 class myGATConv(nn.Module):
     """
     Adapted from
     https://docs.dgl.ai/_modules/dgl/nn/pytorch/conv/gatconv.html#GATConv
     """
-    def __init__(self,
-                 edge_feats,
-                 num_etypes,
-                 in_feats,
-                 out_feats,
-                 num_heads,
-                 feat_drop=0.,
-                 attn_drop=0.,
-                 negative_slope=0.2,
-                 residual=False,
-                 activation=None,
-                 allow_zero_in_degree=False,
-                 bias=False,
-                 alpha=0.):
+
+    def __init__(
+        self,
+        edge_feats,
+        num_etypes,
+        in_feats,
+        out_feats,
+        num_heads,
+        feat_drop=0.0,
+        attn_drop=0.0,
+        negative_slope=0.2,
+        residual=False,
+        activation=None,
+        allow_zero_in_degree=False,
+        bias=False,
+        alpha=0.0,
+    ):
         super(myGATConv, self).__init__()
         self._edge_feats = edge_feats
         self._num_heads = num_heads
@@ -37,14 +41,11 @@ class myGATConv(nn.Module):
         self._allow_zero_in_degree = allow_zero_in_degree
         self.edge_emb = nn.Embedding(num_etypes, edge_feats)
         if isinstance(in_feats, tuple):
-            self.fc_src = nn.Linear(
-                self._in_src_feats, out_feats * num_heads, bias=False)
-            self.fc_dst = nn.Linear(
-                self._in_dst_feats, out_feats * num_heads, bias=False)
+            self.fc_src = nn.Linear(self._in_src_feats, out_feats * num_heads, bias=False)
+            self.fc_dst = nn.Linear(self._in_dst_feats, out_feats * num_heads, bias=False)
         else:
-            self.fc = nn.Linear(
-                self._in_src_feats, out_feats * num_heads, bias=False)
-        self.fc_e = nn.Linear(edge_feats, edge_feats*num_heads, bias=False)
+            self.fc = nn.Linear(self._in_src_feats, out_feats * num_heads, bias=False)
+        self.fc_e = nn.Linear(edge_feats, edge_feats * num_heads, bias=False)
         self.attn_l = nn.Parameter(th.FloatTensor(size=(1, num_heads, out_feats)))
         self.attn_r = nn.Parameter(th.FloatTensor(size=(1, num_heads, out_feats)))
         self.attn_e = nn.Parameter(th.FloatTensor(size=(1, num_heads, edge_feats)))
@@ -53,12 +54,11 @@ class myGATConv(nn.Module):
         self.leaky_relu = nn.LeakyReLU(negative_slope)
         if residual:
             if self._in_dst_feats != out_feats:
-                self.res_fc = nn.Linear(
-                    self._in_dst_feats, num_heads * out_feats, bias=False)
+                self.res_fc = nn.Linear(self._in_dst_feats, num_heads * out_feats, bias=False)
             else:
                 self.res_fc = Identity()
         else:
-            self.register_buffer('res_fc', None)
+            self.register_buffer("res_fc", None)
         self.reset_parameters()
         self.activation = activation
         self.bias = bias
@@ -67,8 +67,8 @@ class myGATConv(nn.Module):
         self.alpha = alpha
 
     def reset_parameters(self):
-        gain = nn.init.calculate_gain('relu')
-        if hasattr(self, 'fc'):
+        gain = nn.init.calculate_gain("relu")
+        if hasattr(self, "fc"):
             nn.init.xavier_normal_(self.fc.weight, gain=gain)
         else:
             nn.init.xavier_normal_(self.fc_src.weight, gain=gain)
@@ -87,47 +87,47 @@ class myGATConv(nn.Module):
         with graph.local_scope():
             if not self._allow_zero_in_degree:
                 if (graph.in_degrees() == 0).any():
-                    raise DGLError('There are 0-in-degree nodes in the graph, '
-                                   'output for those nodes will be invalid. '
-                                   'This is harmful for some applications, '
-                                   'causing silent performance regression. '
-                                   'Adding self-loop on the input graph by '
-                                   'calling `g = dgl.add_self_loop(g)` will resolve '
-                                   'the issue. Setting ``allow_zero_in_degree`` '
-                                   'to be `True` when constructing this module will '
-                                   'suppress the check and let the code run.')
+                    raise DGLError(
+                        "There are 0-in-degree nodes in the graph, "
+                        "output for those nodes will be invalid. "
+                        "This is harmful for some applications, "
+                        "causing silent performance regression. "
+                        "Adding self-loop on the input graph by "
+                        "calling `g = dgl.add_self_loop(g)` will resolve "
+                        "the issue. Setting ``allow_zero_in_degree`` "
+                        "to be `True` when constructing this module will "
+                        "suppress the check and let the code run."
+                    )
 
             if isinstance(feat, tuple):
                 h_src = self.feat_drop(feat[0])
                 h_dst = self.feat_drop(feat[1])
-                if not hasattr(self, 'fc_src'):
+                if not hasattr(self, "fc_src"):
                     self.fc_src, self.fc_dst = self.fc, self.fc
                 feat_src = self.fc_src(h_src).view(-1, self._num_heads, self._out_feats)
                 feat_dst = self.fc_dst(h_dst).view(-1, self._num_heads, self._out_feats)
             else:
                 h_src = h_dst = self.feat_drop(feat)
-                feat_src = feat_dst = self.fc(h_src).view(
-                    -1, self._num_heads, self._out_feats)
+                feat_src = feat_dst = self.fc(h_src).view(-1, self._num_heads, self._out_feats)
                 if graph.is_block:
-                    feat_dst = feat_src[:graph.number_of_dst_nodes()]
+                    feat_dst = feat_src[: graph.number_of_dst_nodes()]
             e_feat = self.edge_emb(e_feat)
             e_feat = self.fc_e(e_feat).view(-1, self._num_heads, self._edge_feats)
             ee = (e_feat * self.attn_e).sum(dim=-1).unsqueeze(-1)
             el = (feat_src * self.attn_l).sum(dim=-1).unsqueeze(-1)
             er = (feat_dst * self.attn_r).sum(dim=-1).unsqueeze(-1)
-            graph.srcdata.update({'ft': feat_src, 'el': el})
-            graph.dstdata.update({'er': er})
-            graph.edata.update({'ee': ee})
-            graph.apply_edges(fn.u_add_v('el', 'er', 'e'))
-            e = self.leaky_relu(graph.edata.pop('e')+graph.edata.pop('ee'))
+            graph.srcdata.update({"ft": feat_src, "el": el})
+            graph.dstdata.update({"er": er})
+            graph.edata.update({"ee": ee})
+            graph.apply_edges(fn.u_add_v("el", "er", "e"))
+            e = self.leaky_relu(graph.edata.pop("e") + graph.edata.pop("ee"))
             # compute softmax
-            graph.edata['a'] = self.attn_drop(edge_softmax(graph, e))
+            graph.edata["a"] = self.attn_drop(edge_softmax(graph, e))
             if res_attn is not None:
-                graph.edata['a'] = graph.edata['a'] * (1-self.alpha) + res_attn * self.alpha
+                graph.edata["a"] = graph.edata["a"] * (1 - self.alpha) + res_attn * self.alpha
             # message passing
-            graph.update_all(fn.u_mul_e('ft', 'a', 'm'),
-                             fn.sum('m', 'ft'))
-            rst = graph.dstdata['ft']
+            graph.update_all(fn.u_mul_e("ft", "a", "m"), fn.sum("m", "ft"))
+            rst = graph.dstdata["ft"]
             # residual
             if self.res_fc is not None:
                 resval = self.res_fc(h_dst).view(h_dst.shape[0], -1, self._out_feats)
@@ -138,6 +138,4 @@ class myGATConv(nn.Module):
             # activation
             if self.activation:
                 rst = self.activation(rst)
-            return rst, graph.edata.pop('a').detach()
-
-
+            return rst, graph.edata.pop("a").detach()
--- examples/simple_hgn/run.py
+++ examples/simple_hgn/run.py
@@ -27,6 +27,7 @@ class SimpleHGN(BaseModel):
         hidden_size (int) : The dimension of node representation.
         dropout (float) : Dropout rate for model training.
     """
+
     @staticmethod
     def add_args(parser):
         """Add model-specific arguments to the parser."""
@@ -48,7 +49,7 @@ class SimpleHGN(BaseModel):
         heads = [args.num_heads] * args.num_layers + [1]
         return cls(
             args.edge_dim,
-            args.num_edge*2+1,
+            args.num_edge * 2 + 1,
             [args.num_features],
             args.hidden_size,
             args.num_classes,
@@ -59,47 +60,85 @@ class SimpleHGN(BaseModel):
             args.slope,
             True,
             0.05,
-            True
+            True,
         )
 
-    def __init__(self,
-                 edge_dim,
-                 num_etypes,
-                 in_dims,
-                 num_hidden,
-                 num_classes,
-                 num_layers,
-                 heads,
-                 feat_drop,
-                 attn_drop,
-                 negative_slope,
-                 residual,
-                 alpha,
-                 use_cuda):
+    def __init__(
+        self,
+        edge_dim,
+        num_etypes,
+        in_dims,
+        num_hidden,
+        num_classes,
+        num_layers,
+        heads,
+        feat_drop,
+        attn_drop,
+        negative_slope,
+        residual,
+        alpha,
+        use_cuda,
+    ):
         super(SimpleHGN, self).__init__()
         self.cross_entropy_loss = nn.CrossEntropyLoss()
-        self.device = torch.device('cuda:0' if torch.cuda.is_available() and use_cuda else 'cpu')
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() and use_cuda else "cpu")
         self.g = None
         self.num_layers = num_layers
         self.gat_layers = nn.ModuleList()
         self.activation = F.elu
-        #self.fc_list = nn.ModuleList([nn.Linear(in_dim, num_hidden, bias=True) for in_dim in in_dims])
-        #for fc in self.fc_list:
+        # self.fc_list = nn.ModuleList([nn.Linear(in_dim, num_hidden, bias=True) for in_dim in in_dims])
+        # for fc in self.fc_list:
         #    nn.init.xavier_normal_(fc.weight, gain=1.414)
         # input projection (no residual)
-        self.gat_layers.append(myGATConv(edge_dim, num_etypes,
-            in_dims[0], num_hidden, heads[0],
-            feat_drop, attn_drop, negative_slope, False, self.activation, alpha=alpha))
+        self.gat_layers.append(
+            myGATConv(
+                edge_dim,
+                num_etypes,
+                in_dims[0],
+                num_hidden,
+                heads[0],
+                feat_drop,
+                attn_drop,
+                negative_slope,
+                False,
+                self.activation,
+                alpha=alpha,
+            )
+        )
         # hidden layers
-        for l in range(1, num_layers):
+        for l in range(1, num_layers):  # noqa E741
             # due to multi-head, the in_dim = num_hidden * num_heads
-            self.gat_layers.append(myGATConv(edge_dim, num_etypes,
-                num_hidden * heads[l-1], num_hidden, heads[l],
-                feat_drop, attn_drop, negative_slope, residual, self.activation, alpha=alpha))
+            self.gat_layers.append(
+                myGATConv(
+                    edge_dim,
+                    num_etypes,
+                    num_hidden * heads[l - 1],
+                    num_hidden,
+                    heads[l],
+                    feat_drop,
+                    attn_drop,
+                    negative_slope,
+                    residual,
+                    self.activation,
+                    alpha=alpha,
+                )
+            )
         # output projection
-        self.gat_layers.append(myGATConv(edge_dim, num_etypes,
-            num_hidden * heads[-2], num_classes, heads[-1],
-            feat_drop, attn_drop, negative_slope, residual, None, alpha=alpha))
+        self.gat_layers.append(
+            myGATConv(
+                edge_dim,
+                num_etypes,
+                num_hidden * heads[-2],
+                num_classes,
+                heads[-1],
+                feat_drop,
+                attn_drop,
+                negative_slope,
+                residual,
+                None,
+                alpha=alpha,
+            )
+        )
         self.epsilon = torch.FloatTensor([1e-12]).to(self.device)
 
     def list_to_sp_mat(self, edges, weights):
@@ -107,8 +146,8 @@ class SimpleHGN(BaseModel):
         i = [x for x in edges[0]]
         j = [x for x in edges[1]]
         total = max(max(i), max(j)) + 1
-        return sp.coo_matrix((data, (i,j)), shape=(total, total)).tocsr()
-    
+        return sp.coo_matrix((data, (i, j)), shape=(total, total)).tocsr()
+
     def build_g_feat(self, A):
         edge2type = {}
         edges = []
@@ -116,8 +155,8 @@ class SimpleHGN(BaseModel):
         for k, mat in enumerate(A):
             edges.append(mat[0].cpu().numpy())
             weights.append(mat[1].cpu().numpy())
-            for u,v in zip(*edges[-1]):
-                edge2type[(u,v)] = k
+            for u, v in zip(*edges[-1]):
+                edge2type[(u, v)] = k
         edges = np.concatenate(edges, axis=1)
         weights = np.concatenate(weights)
         adjM = self.list_to_sp_mat(edges, weights)
@@ -129,20 +168,20 @@ class SimpleHGN(BaseModel):
         for u, v in zip(*g.edges()):
             u = u.cpu().item()
             v = v.cpu().item()
-            e_feat.append(edge2type[(u,v)])
+            e_feat.append(edge2type[(u, v)])
         e_feat = torch.tensor(e_feat, dtype=torch.long).to(self.device)
         self.g = g
         self.e_feat = e_feat
 
-    def forward(self, A, X, target_x, target): #features_list, e_feat):
-        #h = []
-        #for fc, feature in zip(self.fc_list, [X]):
+    def forward(self, A, X, target_x, target):  # features_list, e_feat):
+        # h = []
+        # for fc, feature in zip(self.fc_list, [X]):
         #    h.append(fc(feature))
-        h = X#torch.cat(h, 0)
+        h = X  # torch.cat(h, 0)
         if self.g is None:
             self.build_g_feat(A)
         res_attn = None
-        for l in range(self.num_layers):
+        for l in range(self.num_layers):  # noqa E741
             h, res_attn = self.gat_layers[l](self.g, h, self.e_feat, res_attn=res_attn)
             h = h.flatten(1)
         # output projection
@@ -170,4 +209,4 @@ if __name__ == "__main__":
     args, _ = parser.parse_known_args()
     args = options.parse_args_and_arch(parser, args)
     experiment(task="heterogeneous_node_classification", dataset="gtn-acm", model="simple_hgn", args=args)
-    #experiment(task="node_classification", dataset="cora", model="mygcn")
+    # experiment(task="node_classification", dataset="cora", model="mygcn")

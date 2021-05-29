70f1188198d8370e6756a08357a5564c70b9d2d1
Kaixuan Ji <jkx19@mails.tsinghua.edu.cn>
2021-05-29 15:28:00 +0800

    fixed a bug

--- tests/datasets/test_oagbert_data.py
+++ tests/datasets/test_oagbert_data.py
@@ -4,9 +4,9 @@ from cogdl.utils import build_args_from_dict
 
 def test_oagbert_dataset():
     args = build_args_from_dict({'dataset': 'aff30'})
-    dataset = build_dataset(args)
-    assert isinstance(dataset.get_data(), dict)
-    assert isinstance(dataset.get_candidates(), list)
+    data = build_dataset(args)
+    assert isinstance(data.get_data(), dict)
+    assert isinstance(data.get_candidates(), list)
 
 if __name__ == '__main__':
     test_oagbert_dataset()

7d80350b8e1f944a564e9b60f1d663d984a8b716
THINK2TRY <48679723+THINK2TRY@users.noreply.github.com>
2021-07-10 10:08:10 +0800

    modify moe_gcn

--- cogdl/models/nn/moe_gcn.py
+++ cogdl/models/nn/moe_gcn.py
@@ -38,7 +38,7 @@ class GraphConv(nn.Module):
     Simple GCN layer, similar to https://arxiv.org/abs/1609.02907
     """
 
-    def __init__(self, in_features, out_features, dropout=0.0, activation=None, residual=False, norm=None, bias=True):
+    def __init__(self, in_features, out_features, dropout=0.0, residual=False, norm=None, bias=True):
         super(GraphConv, self).__init__()
         self.in_features = in_features
         self.out_features = out_features
@@ -51,11 +51,6 @@ class GraphConv(nn.Module):
             self.residual = nn.Linear(in_features, out_features)
         else:
             self.residual = None
-
-        if activation is not None:
-            self.act = get_activation(activation)
-        else:
-            self.act = None
 
         if norm is not None:
             if norm == "batchnorm":
@@ -84,44 +79,47 @@ class GraphConv(nn.Module):
         out = spmm(graph, support)
         if self.bias is not None:
             out = out + self.bias
-        if self.norm is not None:
-            out = self.norm(out)
 
         if self.residual is not None:
-            out = out + self.residual(x)
-
-        if self.act is not None:
-            out = self.act(out)
+            res = self.residual(x)
+            if self.act is not None:
+                res = self.act(res)
+            out = out + res
 
         if self.dropout is not None:
             out = self.dropout(out)
+
+        if self.norm is not None:
+            out = self.norm(out)
+
         return out
 
 
 class GraphConvBlock(nn.Module):
-    def __init__(self, in_feats, out_feats, dropout=0.0, residual=False, norm=None, activation=None):
+    def __init__(self, conv_func, conv_params, out_feats, dropout=0.0, in_feats=None, residual=False):
         super(GraphConvBlock, self).__init__()
 
-        self.activation = get_activation(activation)
-        self.graph_conv = GraphConv(in_feats, out_feats, residual=residual, norm=norm, activation=activation)
-        if dropout > 0:
-            self.dropout = nn.Dropout(dropout)
-        else:
-            self.dropout = nn.Identity()
+        self.graph_conv = conv_func(**conv_params)
         self.pos_ff = CustomizedMoEPositionwiseFF(out_feats, out_feats * 2, dropout, moe_num_expert=64, moe_top_k=2)
-        self.act = get_activation(activation)
+        self.dropout = dropout
+        if residual is True:
+            assert in_feats is not None
+            self.res_connection = nn.Linear(in_feats, out_feats)
+        else:
+            self.res_connection = None
 
     def reset_parameters(self):
         """Reinitialize model parameters."""
-        self.graph_conv.reset_parameters()
+        # self.graph_conv.reset_parameters()
         self.res_connection.reset_parameters()
 
     def forward(self, graph, feats):
         new_feats = self.graph_conv(graph, feats)
-
-        new_feats = self.dropout(new_feats)
+        if self.res_connection is not None:
+            res = self.res_connection
+            new_feats = new_feats + res
+            new_feats = F.dropout(new_feats, p=self.dropout, training=self.training)
         new_feats = self.pos_ff(new_feats)
-        new_feats = self.act(new_feats)
 
         return new_feats
 
@@ -147,8 +145,8 @@ class MoEGCN(BaseModel):
         parser.add_argument("--num-layers", type=int, default=2)
         parser.add_argument("--hidden-size", type=int, default=64)
         parser.add_argument("--dropout", type=float, default=0.5)
-        parser.add_argument("--residual", action="store_true")
-        parser.add_argument("--norm", type=str, default=None)
+        parser.add_argument("--no-residual", action="store_true")
+        parser.add_argument("--norm", type=str, default="batchnorm")
         parser.add_argument("--activation", type=str, default="relu")
         # fmt: on
 
@@ -161,30 +159,38 @@ class MoEGCN(BaseModel):
             args.num_layers,
             args.dropout,
             args.activation,
-            args.residual,
+            not args.no_residual,
             args.norm,
         )
 
     def __init__(
-        self, in_feats, hidden_size, out_feats, num_layers, dropout, activation="relu", residual=False, norm=None
+        self, in_feats, hidden_size, out_feats, num_layers, dropout, activation="relu", residual=True, norm=None
     ):
         super(MoEGCN, self).__init__()
         shapes = [in_feats] + [hidden_size] * (num_layers - 1) + [out_feats]
+        conv_func = GraphConv
+        conv_params = {
+            "in_features": in_feats,
+            "out_features": out_feats,
+            "dropout": dropout,
+            "norm": norm,
+            "residual": residual,
+            "activation": activation,
+        }
         self.layers = nn.ModuleList(
             [
                 GraphConvBlock(
-                    shapes[i],
+                    conv_func,
+                    conv_params,
                     shapes[i + 1],
                     dropout=dropout if i != num_layers - 1 else 0,
-                    residual=residual if i != num_layers - 1 else None,
-                    norm=norm if i != num_layers - 1 else None,
-                    activation=activation if i != num_layers - 1 else None,
                 )
                 for i in range(num_layers)
             ]
         )
         self.num_layers = num_layers
         self.dropout = dropout
+        self.act = get_activation(activation)
 
     def get_embeddings(self, graph):
         graph.sym_norm()
@@ -200,6 +206,9 @@ class MoEGCN(BaseModel):
         h = graph.x
         for i in range(self.num_layers):
             h = self.layers[i](graph, h)
+            if i != self.num_layers - 1:
+                h = self.act(h)
+                h = F.dropout(h, self.dropout, training=self.training)
         return h
 
     def predict(self, data):

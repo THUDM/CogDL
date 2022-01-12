881746a69b74d3667e2ce59bcbcf6a5835d52751
Yukuo Cen <cenyk1230@qq.com>
2022-01-12 18:11:50 +0800

    [Example] Add GAT for ogbn-arxiv dataset (#327)

--- examples/ogb/arxiv/gnn.py
+++ examples/ogb/arxiv/gnn.py
@@ -1,8 +1,9 @@
 import argparse
 import torch.nn as nn
+import torch.nn.functional as F
 from cogdl import experiment
 from cogdl.models import BaseModel
-from cogdl.layers import GCNLayer, SAGELayer
+from cogdl.layers import GCNLayer, SAGELayer, GATLayer
 from cogdl.datasets.ogb import OGBArxivDataset
 
 
@@ -80,6 +81,68 @@ class SAGE(BaseModel):
         return x
 
 
+class GAT(BaseModel):
+    def __init__(
+        self,
+        in_feats,
+        hidden_size,
+        out_feats,
+        num_layers,
+        dropout=0.5,
+        input_drop=0.25,
+        attn_drop=0.0,
+        alpha=0.2,
+        nhead=4,
+        residual=True,
+        last_nhead=1,
+        norm="batchnorm",
+    ):
+        """Sparse version of GAT."""
+        super(GAT, self).__init__()
+        self.dropout = dropout
+        self.input_drop = input_drop
+        self.attentions = nn.ModuleList()
+        self.attentions.append(
+            GATLayer(in_feats, hidden_size, nhead=nhead, attn_drop=attn_drop, alpha=alpha, residual=residual, norm=norm)
+        )
+        for i in range(num_layers - 2):
+            self.attentions.append(
+                GATLayer(
+                    hidden_size * nhead,
+                    hidden_size,
+                    nhead=nhead,
+                    attn_drop=attn_drop,
+                    alpha=alpha,
+                    residual=residual,
+                    norm=norm,
+                )
+            )
+        self.attentions.append(
+            GATLayer(
+                hidden_size * nhead,
+                out_feats,
+                attn_drop=attn_drop,
+                alpha=alpha,
+                nhead=last_nhead,
+                residual=False,
+            )
+        )
+        self.num_layers = num_layers
+        self.last_nhead = last_nhead
+        self.residual = residual
+
+    def forward(self, graph):
+        graph.sym_norm()
+        x = graph.x
+        x = F.dropout(x, p=self.input_drop, training=self.training)
+        for i, layer in enumerate(self.attentions):
+            x = layer(graph, x)
+            if i != self.num_layers - 1:
+                x = F.relu(x)
+                x = F.dropout(x, p=self.dropout, training=self.training)
+        return x
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="OGBN-Arxiv (CogDL GNNs)")
     parser.add_argument("--gnn", type=str, default="gcn")
@@ -88,6 +151,7 @@ if __name__ == "__main__":
     parser.add_argument("--dropout", type=float, default=0.5)
     parser.add_argument("--lr", type=float, default=0.01)
     parser.add_argument("--weight-decay", type=float, default=0.0)
+    parser.add_argument("--nhead", type=int, default=3)
     parser.add_argument("--epochs", type=int, default=500)
     parser.add_argument("--runs", type=int, default=10)
     args = parser.parse_args()
@@ -108,6 +172,15 @@ if __name__ == "__main__":
             out_feats=dataset.num_classes,
             num_layers=args.num_layers,
             dropout=args.dropout,
+        )
+    elif args.gnn == "gat":
+        gnn = GAT(
+            in_feats=dataset.num_features,
+            hidden_size=args.hidden_size,
+            out_feats=dataset.num_classes,
+            num_layers=args.num_layers,
+            dropout=args.dropout,
+            nhead=args.nhead,
         )
 
     experiment(

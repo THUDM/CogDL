cee18ade178bbea49d5896fef238acb343271439
xu-ll18 <2621922727@qq.com>
2022-06-21 21:29:26 +0800

    modify grb pytest

--- tests/tasks/test_attack_defense.py
+++ tests/tasks/test_attack_defense.py
@@ -39,7 +39,49 @@ def train_model(model, graph, dataset, test_mask, device, device_ids):
     return test_score
 
 
-def test_dice_modification_attack(model_sur, model_target, graph, test_mask, device):
+def init_dataset():
+    dataset = Cora_GRBDataset()
+    graph = copy.deepcopy(dataset.get(0))
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    device_ids = [0]
+    graph.to(device)
+    test_mask = graph.test_mask
+    set_random_seed(40)
+    return graph, dataset, test_mask, device, device_ids
+
+
+def init_surrogate_model(graph, dataset, test_mask, device, device_ids):
+    model_sur = GCN(
+        in_feats=graph.num_features,
+        hidden_size=64,
+        out_feats=graph.num_classes,
+        num_layers=2,
+        dropout=0.5,
+        activation=None
+    )
+    score_sur = train_model(model_sur, graph, dataset, test_mask, device, device_ids)
+    print("Test score before attack for surrogate model: {:.4f}.".format(score_sur))
+    return model_sur
+
+
+def init_target_model(graph, dataset, test_mask, device, device_ids):
+    model_target = GCN(
+        in_feats=graph.num_features,
+        hidden_size=64,
+        out_feats=graph.num_classes,
+        num_layers=3,
+        dropout=0.5,
+        activation="relu"
+    )
+    score_target = train_model(model_target, graph, dataset, test_mask, device, device_ids)
+    print("Test score before attack for target model: {:.4f}.".format(score_target))
+    return model_target
+
+
+def test_dice_modification_attack():
+    graph, dataset, test_mask, device, device_ids = init_dataset()
+    model_sur = init_surrogate_model(graph, dataset, test_mask, device, device_ids)
+    model_target = init_target_model(graph, dataset, test_mask, device, device_ids)
     print("DICE modification attack...")
     n_mod_ratio = 0.3
     ratio_delete = 0.6
@@ -59,7 +101,10 @@ def test_dice_modification_attack(model_sur, model_target, graph, test_mask, dev
     print("After attack, test score of target model: {:.4f}".format(test_score))
 
 
-def test_fga_modification_attack(model_sur, model_target, graph, test_mask, device):
+def test_fga_modification_attack():
+    graph, dataset, test_mask, device, device_ids = init_dataset()
+    model_sur = init_surrogate_model(graph, dataset, test_mask, device, device_ids)
+    model_target = init_target_model(graph, dataset, test_mask, device, device_ids)
     print("FGA modification attack...")
     n_mod_ratio = 0.1
     attack = FGA(int(graph.to_scipy_csr()[test_mask.cpu()].getnnz() * n_mod_ratio),
@@ -77,7 +122,10 @@ def test_fga_modification_attack(model_sur, model_target, graph, test_mask, devi
     print("After attack, test score of surrogate model: {:.4f}".format(test_score))
 
 
-def test_flip_modification_attack(model_sur, model_target, graph, test_mask, device):
+def test_flip_modification_attack():
+    graph, dataset, test_mask, device, device_ids = init_dataset()
+    model_sur = init_surrogate_model(graph, dataset, test_mask, device, device_ids)
+    model_target = init_target_model(graph, dataset, test_mask, device, device_ids)
     print("FLIP modification attack...")
     n_mod_ratio = 0.01
     n_edge_mod = int(graph.to_scipy_csr()[test_mask.cpu()].getnnz() * n_mod_ratio)
@@ -101,7 +149,10 @@ def test_flip_modification_attack(model_sur, model_target, graph, test_mask, dev
         print("After {} attack, test score of target model: {:.4f}".format(key, test_score))
 
 
-def test_rand_modification_attack(model_sur, model_target, graph, test_mask, device):
+def test_rand_modification_attack():
+    graph, dataset, test_mask, device, device_ids = init_dataset()
+    model_sur = init_surrogate_model(graph, dataset, test_mask, device, device_ids)
+    model_target = init_target_model(graph, dataset, test_mask, device, device_ids)
     print("RAND modification attack...")
     n_mod_ratio = 0.1
     n_edge_mod = int(graph.to_scipy_csr()[test_mask.cpu()].getnnz() * n_mod_ratio)
@@ -120,7 +171,10 @@ def test_rand_modification_attack(model_sur, model_target, graph, test_mask, dev
     print("After attack, test score of target model: {:.4f}".format(test_score))
 
 
-def test_nea_modification_attack(model_sur, model_target, graph, test_mask, device):
+def test_nea_modification_attack():
+    graph, dataset, test_mask, device, device_ids = init_dataset()
+    model_sur = init_surrogate_model(graph, dataset, test_mask, device, device_ids)
+    model_target = init_target_model(graph, dataset, test_mask, device, device_ids)
     print("NEA modification attack...")
     n_mod_ratio = 0.1
     n_edge_mod = int(graph.to_scipy_csr()[test_mask.cpu()].getnnz() * n_mod_ratio)
@@ -139,7 +193,10 @@ def test_nea_modification_attack(model_sur, model_target, graph, test_mask, devi
     print("After attack, test score of target model: {:.4f}".format(test_score))
 
 
-def test_stack_modification_attack(model_sur, model_target, graph, test_mask, device):
+def test_stack_modification_attack():
+    graph, dataset, test_mask, device, device_ids = init_dataset()
+    model_sur = init_surrogate_model(graph, dataset, test_mask, device, device_ids)
+    model_target = init_target_model(graph, dataset, test_mask, device, device_ids)
     print("STACK modification attack...")
     n_mod_ratio = 0.01
     n_edge_mod = int(graph.to_scipy_csr()[test_mask.cpu()].getnnz() * n_mod_ratio)
@@ -158,7 +215,10 @@ def test_stack_modification_attack(model_sur, model_target, graph, test_mask, de
     print("After attack, test score of target model: {:.4f}".format(test_score))
 
 
-def test_pgd_modification_attack(model_sur, model_target, graph, test_mask, device):
+def test_pgd_modification_attack():
+    graph, dataset, test_mask, device, device_ids = init_dataset()
+    model_sur = init_surrogate_model(graph, dataset, test_mask, device, device_ids)
+    model_target = init_target_model(graph, dataset, test_mask, device, device_ids)
     print("PGD modification attack...")
     epsilon = 0.1
     n_epoch = 500
@@ -190,7 +250,10 @@ def test_pgd_modification_attack(model_sur, model_target, graph, test_mask, devi
     print("After attack, test score of target model: {:.4f}".format(test_score))
 
 
-def test_prbcd_modification_attack(model_sur, model_target, graph, test_mask):
+def test_prbcd_modification_attack():
+    graph, dataset, test_mask, device, device_ids = init_dataset()
+    model_sur = init_surrogate_model(graph, dataset, test_mask, device, device_ids)
+    model_target = init_target_model(graph, dataset, test_mask, device, device_ids)
     print("PRBCD modification attack...")
     device = "cpu"
     epsilon = 0.3
@@ -225,7 +288,10 @@ def test_prbcd_modification_attack(model_sur, model_target, graph, test_mask):
     print("After attack, test score of target model: {:.4f}".format(test_score))
 
 
-def test_fgsm_injection_attack(model_sur, model_target, graph, test_mask, device):
+def test_fgsm_injection_attack():
+    graph, dataset, test_mask, device, device_ids = init_dataset()
+    model_sur = init_surrogate_model(graph, dataset, test_mask, device, device_ids)
+    model_target = init_target_model(graph, dataset, test_mask, device, device_ids)
     print("FGSM injection attack...")
     attack = FGSM(epsilon=0.01,
                 n_epoch=1000,
@@ -250,7 +316,10 @@ def test_fgsm_injection_attack(model_sur, model_target, graph, test_mask, device
     print("Test score after attack for target model: {:.4f}.".format(test_score_target_attack))
 
 
-def test_pgd_injection_attack(model_sur, model_target, graph, test_mask, device):
+def test_pgd_injection_attack():
+    graph, dataset, test_mask, device, device_ids = init_dataset()
+    model_sur = init_surrogate_model(graph, dataset, test_mask, device, device_ids)
+    model_target = init_target_model(graph, dataset, test_mask, device, device_ids)
     print("PGD injection attack...")
     attack = PGD(epsilon=0.01,
                 n_epoch=1000,
@@ -275,7 +344,10 @@ def test_pgd_injection_attack(model_sur, model_target, graph, test_mask, device)
     print("Test score after attack for target model: {:.4f}.".format(test_score_target_attack))
 
 
-def test_rand_injection_attack(model_sur, model_target, graph, test_mask, device):
+def test_rand_injection_attack():
+    graph, dataset, test_mask, device, device_ids = init_dataset()
+    model_sur = init_surrogate_model(graph, dataset, test_mask, device, device_ids)
+    model_target = init_target_model(graph, dataset, test_mask, device, device_ids)
     print("RAND injection attack...")
     attack = RAND_Inject(n_inject_max=100,
                 n_edge_max=200,
@@ -298,7 +370,10 @@ def test_rand_injection_attack(model_sur, model_target, graph, test_mask, device
     print("Test score after attack for target model: {:.4f}.".format(test_score_target_attack))
 
 
-def test_speit_injection_attack(model_sur, model_target, graph, test_mask, device):
+def test_speit_injection_attack():
+    graph, dataset, test_mask, device, device_ids = init_dataset()
+    model_sur = init_surrogate_model(graph, dataset, test_mask, device, device_ids)
+    model_target = init_target_model(graph, dataset, test_mask, device, device_ids)
     print("SPEIT injection attack...")
     attack = SPEIT(lr=0.02,
                 n_epoch=1000,
@@ -323,7 +398,10 @@ def test_speit_injection_attack(model_sur, model_target, graph, test_mask, devic
     print("Test score after attack for target model: {:.4f}.".format(test_score_target_attack))
 
 
-def test_tdgia_injection_attack(model_sur, model_target, graph, test_mask, device):
+def test_tdgia_injection_attack():
+    graph, dataset, test_mask, device, device_ids = init_dataset()
+    model_sur = init_surrogate_model(graph, dataset, test_mask, device, device_ids)
+    model_target = init_target_model(graph, dataset, test_mask, device, device_ids)
     print("TDGIA injection attack...")
     attack = TDGIA(lr=0.01,
                 n_epoch=1000,
@@ -351,7 +429,41 @@ def test_tdgia_injection_attack(model_sur, model_target, graph, test_mask, devic
     print("Test score after attack for target model: {:.4f}.".format(test_score_target_attack))
 
 
-def test_gcnsvd_defense(model_sur, graph, dataset, test_mask, device, device_ids):
+def apply_pgd_modification_attack(model_sur, model_target, graph, test_mask, device):
+    print("PGD modification attack...")
+    epsilon = 0.1
+    n_epoch = 500
+    n_mod_ratio = 0.01
+    n_node_mod = int(graph.y.shape[0] * n_mod_ratio)
+    n_edge_mod = int(graph.to_scipy_csr()[test_mask.cpu()].getnnz() * n_mod_ratio)
+    feat_lim_min = 0.0
+    feat_lim_max = 1.0
+    early_stop_patience = 200
+    attack = PGD(epsilon,
+                n_epoch,
+                n_node_mod,
+                n_edge_mod,
+                feat_lim_min,
+                feat_lim_max,
+                early_stop_patience=early_stop_patience,
+                device=device)
+    graph_attack = attack.attack(model_sur, graph)
+    print(graph_attack)
+    test_score = evaluate(model_sur, 
+                        graph_attack,
+                        mask=test_mask,
+                        device=device)
+    print("After attack, test score of surrogate model: {:.4f}".format(test_score))
+    test_score = evaluate(model_target, 
+                        graph_attack,
+                        mask=test_mask,
+                        device=device)
+    print("After attack, test score of target model: {:.4f}".format(test_score))
+
+
+def test_gcnsvd_defense():
+    graph, dataset, test_mask, device, device_ids = init_dataset()
+    model_sur = init_surrogate_model(graph, dataset, test_mask, device, device_ids)
     print("GCNSVD defense model...")
     model_target = GCNSVD(
         in_feats=graph.num_features,
@@ -365,10 +477,12 @@ def test_gcnsvd_defense(model_sur, graph, dataset, test_mask, device, device_ids
     )
     test_score = train_model(model_target, graph, dataset, test_mask, device, device_ids)
     print("Test score before attack for target model: {:.4f}.".format(test_score))
-    test_pgd_modification_attack(model_sur, model_target, graph, test_mask, device)
+    apply_pgd_modification_attack(model_sur, model_target, graph, test_mask, device)
 
 
-def test_gatguard_defense(model_sur, graph, dataset, test_mask, device, device_ids):
+def test_gatguard_defense():
+    graph, dataset, test_mask, device, device_ids = init_dataset()
+    model_sur = init_surrogate_model(graph, dataset, test_mask, device, device_ids)
     print("GATGuard defense model...")
     model_target = GATGuard(in_feats=graph.num_features,
         hidden_size=64,
@@ -380,10 +494,12 @@ def test_gatguard_defense(model_sur, graph, dataset, test_mask, device, device_i
     )
     test_score = train_model(model_target, graph, dataset, test_mask, device, device_ids)
     print("Test score before attack for target model: {:.4f}.".format(test_score))
-    test_pgd_modification_attack(model_sur, model_target, graph, test_mask, device)
+    apply_pgd_modification_attack(model_sur, model_target, graph, test_mask, device)
 
 
-def test_gcnguard_defense(model_sur, graph, dataset, test_mask, device, device_ids):
+def test_gcnguard_defense():
+    graph, dataset, test_mask, device, device_ids = init_dataset()
+    model_sur = init_surrogate_model(graph, dataset, test_mask, device, device_ids)
     print("GATGuard defense model...")
     model_target = GCNGuard(in_feats=graph.num_features,
         hidden_size=64,
@@ -395,10 +511,12 @@ def test_gcnguard_defense(model_sur, graph, dataset, test_mask, device, device_i
     )
     test_score = train_model(model_target, graph, dataset, test_mask, device, device_ids)
     print("Test score before attack for target model: {:.4f}.".format(test_score))
-    test_pgd_modification_attack(model_sur, model_target, graph, test_mask, device)
+    apply_pgd_modification_attack(model_sur, model_target, graph, test_mask, device)
 
 
-def test_robustgcn_defense(model_sur, graph, dataset, test_mask, device, device_ids):
+def test_robustgcn_defense():
+    graph, dataset, test_mask, device, device_ids = init_dataset()
+    model_sur = init_surrogate_model(graph, dataset, test_mask, device, device_ids)
     print("RobustGCN defense model...")
     model_target = RobustGCN(in_feats=graph.num_features,
         hidden_size=128,
@@ -408,10 +526,12 @@ def test_robustgcn_defense(model_sur, graph, dataset, test_mask, device, device_
     )
     test_score = train_model(model_target, graph, dataset, test_mask, device, device_ids)
     print("Test score before attack for target model: {:.4f}.".format(test_score))
-    test_pgd_modification_attack(model_sur, model_target, graph, test_mask, device)
+    apply_pgd_modification_attack(model_sur, model_target, graph, test_mask, device)
 
 
-def test_adversarial_train(model_sur, graph, dataset, test_mask, device, device_ids):
+def test_adversarial_train():
+    graph, dataset, test_mask, device, device_ids = init_dataset()
+    model_sur = init_surrogate_model(graph, dataset, test_mask, device, device_ids)
     model_target = GCN(
         in_feats=graph.num_features,
         hidden_size=64,
@@ -470,48 +590,21 @@ def test_adversarial_train(model_sur, graph, dataset, test_mask, device, device_
 
 
 if __name__ == "__main__":
-    dataset = Cora_GRBDataset()
-    graph = copy.deepcopy(dataset.get(0))
-    device = "cuda:0" if torch.cuda.is_available() else "cpu"
-    device_ids = [0]
-    graph.to(device)
-    test_mask = graph.test_mask
-    set_random_seed(40)
-    model_sur = GCN(
-        in_feats=graph.num_features,
-        hidden_size=64,
-        out_feats=graph.num_classes,
-        num_layers=2,
-        dropout=0.5,
-        activation=None
-    )
-    score_sur = train_model(model_sur, graph, dataset, test_mask, device, device_ids)
-    print("Test score before attack for surrogate model: {:.4f}.".format(score_sur))
-    model_target = GCN(
-        in_feats=graph.num_features,
-        hidden_size=64,
-        out_feats=graph.num_classes,
-        num_layers=3,
-        dropout=0.5,
-        activation="relu"
-    )
-    score_target = train_model(model_sur, graph, dataset, test_mask, device, device_ids)
-    print("Test score before attack for target model: {:.4f}.".format(score_target))
-    test_dice_modification_attack(model_sur, model_target, graph, test_mask, device)
-    test_fga_modification_attack(model_sur, model_target, graph, test_mask, device)
-    test_flip_modification_attack(model_sur, model_target, graph, test_mask, device)
-    test_rand_modification_attack(model_sur, model_target, graph, test_mask, device)
-    test_nea_modification_attack(model_sur, model_target, graph, test_mask, device)
-    test_stack_modification_attack(model_sur, model_target, graph, test_mask, device)
-    test_pgd_modification_attack(model_sur, model_target, graph, test_mask, device)
-    test_prbcd_modification_attack(model_sur, model_target, graph, test_mask)
-    test_fgsm_injection_attack(model_sur, model_target, graph, test_mask, device)
-    test_pgd_injection_attack(model_sur, model_target, graph, test_mask, device)
-    test_rand_injection_attack(model_sur, model_target, graph, test_mask, device)
-    test_speit_injection_attack(model_sur, model_target, graph, test_mask, device)
-    test_tdgia_injection_attack(model_sur, model_target, graph, test_mask, device)
-    test_gcnsvd_defense(model_sur, graph, dataset, test_mask, device, device_ids)
-    test_gatguard_defense(model_sur, graph, dataset, test_mask, device, device_ids)
-    test_gcnguard_defense(model_sur, graph, dataset, test_mask, device, device_ids)
-    test_robustgcn_defense(model_sur, graph, dataset, test_mask, device, device_ids)
-    test_adversarial_train(model_sur, graph, dataset, test_mask, device, device_ids)
+    test_dice_modification_attack()
+    # test_fga_modification_attack()
+    # test_flip_modification_attack()
+    # test_rand_modification_attack()
+    # test_nea_modification_attack()
+    # test_stack_modification_attack()
+    # test_pgd_modification_attack()
+    test_prbcd_modification_attack()
+    test_fgsm_injection_attack()
+    # test_pgd_injection_attack()
+    # test_rand_injection_attack()
+    # test_speit_injection_attack()
+    # test_tdgia_injection_attack()
+    test_gcnsvd_defense()
+    # test_gatguard_defense()
+    # test_gcnguard_defense()
+    # test_robustgcn_defense()
+    test_adversarial_train()

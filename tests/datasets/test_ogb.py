61d9b827e5557cbebc7d7691f76ff8d3dcdc0f96
xinjie zhang <xzha@monanash.edu.com>
2022-07-22 13:04:32 +0800

    update ogbl test

--- tests/datasets/test_ogb.py
+++ tests/datasets/test_ogb.py
@@ -18,7 +18,15 @@ def test_ogbg_molhiv():
     assert dataset.all_nodes == 1049163
     assert len(dataset.data) == 41127
 
+def test_ogbl_ddi():
+    args = build_args_from_dict({"dataset": "ogbl-ddi"})
+    assert args.dataset == "ogbl-ddi"
+    dataset = build_dataset(args)
+    data = dataset.data
+    assert data.num_nodes = 4267
+
 
 if __name__ == "__main__":
     test_ogbn_arxiv()
     test_ogbg_molhiv()
+    test_ogbl_ddi()

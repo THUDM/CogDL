a88170523cd3fba0f4ee38634bb75756d24674f0
QingFei1 <723387223@qq.com>
2022-08-10 22:25:29 +0800

    Update Triple_Link_Prediction

--- cogdl/wrappers/data_wrapper/link_prediction/triple_link_prediction_dw.py
+++ cogdl/wrappers/data_wrapper/link_prediction/triple_link_prediction_dw.py
@@ -1,9 +1,9 @@
 from .. import DataWrapper
 from cogdl.datasets.kg_data import BidirectionalOneShotIterator, TestDataset, TrainDataset
 from torch.utils.data import DataLoader
+
+
 class TripleDataWrapper(DataWrapper):
-
-
     @classmethod
     def add_args(self, parser):
         # fmt: off
@@ -11,28 +11,27 @@ class TripleDataWrapper(DataWrapper):
         parser.add_argument("--test_batch_size", type=int, default=4)
         self.parser = parser
         return self.parser
-        #fmt on
+        # fmt: on
 
     def __init__(self, dataset):
         super(TripleDataWrapper, self).__init__(dataset)
         self.args = self.parser.parse_args()
         self.dataset = dataset
         self.negative_sample_size = self.args.negative_sample_size
-        self.batch_size=self.args.batch_size
-
+        self.batch_size = self.args.batch_size
 
     def train_wrapper(self):
-        dataset=self.dataset
+        dataset = self.dataset
 
-        train_iter=self.output_iter(dataset,self.negative_sample_size,self.batch_size)
+        train_iter = self.output_iter(dataset, self.negative_sample_size, self.batch_size)
         return train_iter
 
     def val_wrapper(self):
         dataset = self.dataset
-        train_triples = dataset.triples[dataset.train_start_idx: dataset.valid_start_idx]
+        train_triples = dataset.triples[dataset.train_start_idx : dataset.valid_start_idx]
 
-        valid_triples = dataset.triples[dataset.valid_start_idx: dataset.test_start_idx]
-        test_triples = dataset.triples[dataset.test_start_idx:]
+        valid_triples = dataset.triples[dataset.valid_start_idx : dataset.test_start_idx]
+        test_triples = dataset.triples[dataset.test_start_idx :]
         all_true_triples = train_triples + valid_triples + test_triples
         test_dataloader_head = DataLoader(
             TestDataset(valid_triples, all_true_triples, dataset.num_entities, dataset.num_relations, "head-batch"),
@@ -46,14 +45,14 @@ class TripleDataWrapper(DataWrapper):
             collate_fn=TestDataset.collate_fn,
         )
 
-        return (test_dataloader_head,test_dataloader_tail)
+        return (test_dataloader_head, test_dataloader_tail)
 
     def test_wrapper(self):
         dataset = self.dataset
-        train_triples = dataset.triples[dataset.train_start_idx: dataset.valid_start_idx]
+        train_triples = dataset.triples[dataset.train_start_idx : dataset.valid_start_idx]
 
-        valid_triples = dataset.triples[dataset.valid_start_idx: dataset.test_start_idx]
-        test_triples = dataset.triples[dataset.test_start_idx:]
+        valid_triples = dataset.triples[dataset.valid_start_idx : dataset.test_start_idx]
+        test_triples = dataset.triples[dataset.test_start_idx :]
         all_true_triples = train_triples + valid_triples + test_triples
         test_dataloader_head = DataLoader(
             TestDataset(test_triples, all_true_triples, dataset.num_entities, dataset.num_relations, "head-batch"),
@@ -66,16 +65,15 @@ class TripleDataWrapper(DataWrapper):
             batch_size=self.args.test_batch_size,
             collate_fn=TestDataset.collate_fn,
         )
-        return (test_dataloader_head,test_dataloader_tail)
+        return (test_dataloader_head, test_dataloader_tail)
+
     @staticmethod
-    def output_iter(dataset,negative_sample_size,batch_size):
-        train_triples =dataset.triples[dataset.train_start_idx: dataset.valid_start_idx]
+    def output_iter(dataset, negative_sample_size, batch_size):
+        train_triples = dataset.triples[dataset.train_start_idx : dataset.valid_start_idx]
 
-        valid_triples = dataset.triples[dataset.valid_start_idx: dataset.test_start_idx]
+        valid_triples = dataset.triples[dataset.valid_start_idx : dataset.test_start_idx]
 
-
-        nentity, nrelation = dataset._num_entities , dataset._num_relations
-
+        nentity, nrelation = dataset._num_entities, dataset._num_relations
 
         # Set training dataloader iterator
         train_dataloader_head = DataLoader(

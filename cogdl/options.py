99acfbd9378ae42cfb59c883e900968d9f3eed8e
QingFei1 <723387223@qq.com>
2022-08-11 10:48:43 +0800

    Update Triple_Link_Prediction

--- cogdl/options.py
+++ cogdl/options.py
@@ -51,6 +51,8 @@ def get_parser():
     parser.add_argument("--actnn", action="store_true")
     parser.add_argument("--fp16", action="store_true")
     parser.add_argument("--rp-ratio", type=int, default=1)
+    parser.add_argument("--do_test", default=True)
+    parser.add_argument("--do_valid", default=True)
     # fmt: on
     return parser
 

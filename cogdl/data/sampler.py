c7f35aa1c5d9444bfb7be9353af69b9eddd3cfb9
QingFei1 <91861679+QingFei1@users.noreply.github.com>
2022-08-22 14:16:44 +0800

    [Feature] Update Unsup_Graphsage (#379)

--- cogdl/data/sampler.py
+++ cogdl/data/sampler.py
@@ -1,3 +1,6 @@
+
+from tracemalloc import start
+from turtle import pos
 from typing import List
 import os
 import random
@@ -5,9 +8,9 @@ import numpy as np
 import scipy.sparse as sp
 import torch
 import torch.utils.data
-
 from cogdl.utils import remove_self_loops, row_normalization
 from cogdl.data import Graph, DataLoader
+from cogdl.utils import RandomWalker
 
 
 class NeighborSampler(DataLoader):
@@ -16,8 +19,7 @@ class NeighborSampler(DataLoader):
             batch_size = kwargs["batch_size"]
         else:
             batch_size = 8
-
-        if isinstance(dataset.data, Graph):
+        if isinstance(dataset.data, Graph):     
             self.dataset = NeighborSamplerDataset(dataset, sizes, batch_size, mask)
         else:
             self.dataset = dataset
@@ -34,6 +36,29 @@ class NeighborSampler(DataLoader):
         self.dataset.shuffle()
 
 
+class UnsupNeighborSampler(DataLoader):
+    def __init__(self, dataset, sizes: List[int], mask=None, **kwargs):
+        if "batch_size" in kwargs:
+            batch_size = kwargs["batch_size"]
+        else:
+            batch_size = 8
+
+        if isinstance(dataset.data, Graph):
+            self.dataset = UnsupNeighborSamplerDataset(dataset, sizes, batch_size, mask)
+        else:
+            self.dataset = dataset
+        kwargs["batch_size"] = 1
+        kwargs["shuffle"] = False
+        kwargs["collate_fn"] = UnsupNeighborSampler.collate_fn
+        super(UnsupNeighborSampler, self).__init__(dataset=self.dataset, **kwargs)
+
+    @staticmethod
+    def collate_fn(data):
+        return data[0]
+
+    def shuffle(self):
+        self.dataset.shuffle()
+
 class NeighborSamplerDataset(torch.utils.data.Dataset):
     def __init__(self, dataset, sizes: List[int], batch_size: int, mask=None):
         super(NeighborSamplerDataset, self).__init__()
@@ -46,6 +71,65 @@ class NeighborSamplerDataset(torch.utils.data.Dataset):
         if mask is not None:
             self.node_idx = self.node_idx[mask]
         self.num_nodes = self.node_idx.shape[0]
+
+    def shuffle(self):
+        idx = torch.randperm(self.num_nodes)
+        self.node_idx = self.node_idx[idx]
+
+    def __len__(self):
+        return (self.num_nodes - 1) // self.batch_size + 1
+    
+    def __getitem__(self, idx):
+        """
+            Sample a subgraph with neighborhood sampling
+        Args:
+            idx: torch.Tensor / np.array
+                Target nodes
+        Returns:
+            if `size` is `[-1,]`,
+                (
+                    source_nodes_id: Tensor,
+                    sampled_edges: Tensor,
+                    (number_of_source_nodes, number_of_target_nodes): Tuple[int]
+                )
+            otherwise,
+                (
+                    target_nodes_id: Tensor
+                    all_sampled_nodes_id: Tensor,
+                    sampled_adjs: List[Tuple(Tensor, Tensor, Tuple[int]]
+                )
+        """
+        batch = self.node_idx[idx * self.batch_size : (idx + 1) * self.batch_size]
+        node_id = batch
+        adj_list = []
+        for size in self.sizes:
+            src_id, graph = self.data.sample_adj(node_id, size, replace=False)
+            size = (len(src_id), len(node_id))
+            adj_list.append((src_id, graph, size))  # src_id, graph, (src_size, target_size)
+            node_id = src_id
+
+        if self.sizes == [-1]:
+            src_id, graph, _ = adj_list[0]
+            size = (len(src_id), len(batch))
+            return src_id, graph, size
+        else:
+            return batch, node_id, adj_list[::-1]
+
+
+class UnsupNeighborSamplerDataset(torch.utils.data.Dataset):
+    def __init__(self, dataset, sizes: List[int], batch_size: int, mask=None):
+        super(UnsupNeighborSamplerDataset, self).__init__()
+        self.data = dataset.data
+        self.x = self.data.x
+        self.edge_index=self.data.edge_index
+        self.sizes = sizes
+        self.batch_size = batch_size
+        self.node_idx = torch.arange(0, self.data.x.shape[0], dtype=torch.long)
+        self.total_num_nodes=self.num_nodes = self.node_idx.shape[0]
+        if mask is not None:
+            self.node_idx = self.node_idx[mask]
+        self.num_nodes = self.node_idx.shape[0]
+        self.random_walker = RandomWalker()
 
     def shuffle(self):
         idx = torch.randperm(self.num_nodes)
@@ -75,6 +159,13 @@ class NeighborSamplerDataset(torch.utils.data.Dataset):
                 )
         """
         batch = self.node_idx[idx * self.batch_size : (idx + 1) * self.batch_size]
+        self.random_walker.build_up(self.edge_index, self.total_num_nodes)
+        walk_res=self.random_walker.walk_one(batch,length=1,p=0.0)
+
+        neg_batch = torch.randint(0, self.total_num_nodes, (batch.numel(), ),
+                                  dtype=torch.int64)        
+        pos_batch=torch.tensor(walk_res)
+        batch = torch.cat([batch, pos_batch, neg_batch], dim=0)
         node_id = batch
         adj_list = []
         for size in self.sizes:
@@ -197,3 +288,4 @@ class RandomPartitionDataset(torch.utils.data.Dataset):
 
     def shuffle(self):
         self.parts = torch.randint(0, self.n_cluster, size=(self.num_nodes,))
+
